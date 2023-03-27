b40afe88816ec381b000fdc2ec3ac173df3ff1b8
Eren Elmacı <erenx1623@gmail.com>
2023-03-27 05:02:37 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { ThemeProvider } from "@emotion/react"
 import { blueGrey, grey } from "@mui/material/colors"
-import { createTheme } from "@mui/system"
+import { createTheme, ThemeProvider } from "@mui/system"
 import { Provider } from "react-redux"
 import "./App.css"
 import AppRouter from "./router/AppRouter"
 import store from "./app/store"
-import {ToastContainer} from "react-toastify"
+import { ToastContainer } from "react-toastify"
 
 function App() {
   const theme = createTheme({
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,34 @@
 import Avatar from "@mui/material/Avatar"
-import CssBaseline from "@mui/material/CssBaseline"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import Grid from "@mui/material/Grid"
 import Box from "@mui/material/Box"
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
+import LockIcon from "@mui/icons-material/Lock"
 import Typography from "@mui/material/Typography"
 import Container from "@mui/material/Container"
 import { createTheme } from "@mui/material/styles"
 import { ThemeProvider } from "@emotion/react"
 import { Form, Formik } from "formik"
 import TextField from "@mui/material/TextField"
-import { object, string, number, date, InferType } from "yup"
-import { display } from "@mui/system"
+import { object, string } from "yup"
+import LoadingButton from "@mui/lab/LoadingButton"
+import { useSelector } from "react-redux"
+import image from "../assets/result.svg"
+import { blueGrey, grey } from "@mui/material/colors"
 
-const theme = createTheme()
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: grey["900"],
+    },
+    secondary: {
+      main: blueGrey["900"],
+    },
+  },
+})
+
 const Login = () => {
-  const handleSubmit = () => {}
+  const navigate = useNavigate()
+  const { currentUser, error, loading } = useSelector((state) => state?.auth)
 
   const loginSchema = object({
     email: string().email("Yanlış email formatı").required("Email zorunludur"),
@@ -25,117 +38,124 @@ const Login = () => {
       .max(30, "Password en fazla 30 karakter uzunluğunda olmalıdır")
       .matches(/\d+/, "Password en az bir sayi içermelidir")
       .matches(/[a-z]/, "Password en az bir küçük harf içermelidir")
-      .matches(/[A-Z]/, "Password en az bir büyük harf içermelidir"),
+      .matches(/[A-Z]/, "Password en az bir büyük harf içermelidir")
+      .matches(
+        /[!,?{}<>%&$#£+-.]/,
+        "Password en az bir özel karakter içermelidir"
+      ),
   })
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <Grid item xs={12} mb={3}>
-          <Typography variant="h3" color="primary" align="center">
-            STOCK APP
-          </Typography>
-        </Grid>
-        <CssBaseline />
-        <Box
+      <Container>
+        <Grid
+          container
+          justifyContent="center"
+          direction="row"
           sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            p: 2,
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
+          <Grid item xs={12} mb={3}>
+            <Typography variant="h3" color="primary" align="center">
+              STOCK APP
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Box>
           <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
+            sx={{
+              display: "flex",
+              flexDirection: { xs: "column", md: "row-reverse" },
+              justifyContent: { xs: "center", md: "space-between" },
+              mt: 8,
+            }}
           >
-            <Formik
-              initialValues={{ email: "", password: "" }}
-              validationSchema={loginSchema}
-              onSubmit={(values, actions) => {
-                actions.resetForm()
-                actions.setSubmitting(false)
-              }}
-            >
-              {({ values, handleChange, handleBlur, errors, touched }) => (
-                <Form>
-                  <Box
-                    sx={{ display: "flex", flexDirection: "column", gap: 2 }}
-                  >
-                    <TextField
-                      label="Email"
-                      name="email"
-                      id="email"
-                      type="email"
-                      variant="outlined"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values?.email || ""}
-                      error={touched.email && Boolean(errors.email)}
-                      helperText={touched.email && errors.email}
-                    />
-                    <TextField
-                      label="Password"
-                      name="password"
-                      id="password"
-                      type="password"
-                      variant="outlined"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values?.password || ""}
-                      error={touched.password && Boolean(errors.password)}
-                      helperText={touched.password && errors.password}
-                    />
-                  </Box>
-                </Form>
-              )}
-            </Formik>
-            {/* <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button> */}
-            <Grid container>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
+            <Box sx={{ width: "50%" }}>
+              <Avatar
+                sx={{
+                  backgroundColor: "secondary.light",
+                  m: "auto",
+                }}
+              >
+                <LockIcon size="30" />
+              </Avatar>
+              <Typography
+                variant="h4"
+                align="center"
+                mb={4}
+                color="secondary.light"
+              >
+                Login
+              </Typography>
+              <Formik
+                initialValues={{ email: "", password: "" }}
+                validationSchema={loginSchema}
+                onSubmit={(values, actions) => {
+                  actions.resetForm()
+                  actions.setSubmitting(false)
+                }}
+              >
+                {({ values, handleChange, handleBlur, errors, touched }) => (
+                  <Form>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: 2,
+                      }}
+                    >
+                      <TextField
+                        label="Email"
+                        name="email"
+                        id="email"
+                        type="email"
+                        variant="outlined"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values?.email || ""}
+                        error={touched.email && Boolean(errors.email)}
+                        helperText={touched.email && errors.email}
+                      />
+                      <TextField
+                        label="Password"
+                        name="password"
+                        id="password"
+                        type="password"
+                        variant="outlined"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values?.password || ""}
+                        error={touched.password && Boolean(errors.password)}
+                        helperText={touched.password && errors.password}
+                      />
+                      <LoadingButton
+                        type="submit"
+                        variant="contained"
+                        loading={loading}
+                      >
+                        Submit
+                      </LoadingButton>
+                    </Box>
+                  </Form>
+                )}
+              </Formik>
+              <Grid sx={{ mt: "1rem", textAlign: "center" }}>
+                <Grid item>
+                  <Link to="/register" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
+            </Box>
+
+            <Box>
+              <Grid width={"30rem"} item xs={10} sm={7} md={6}>
+                <Container>
+                  <img src={image} alt="img" />
+                </Container>
+              </Grid>
+            </Box>
           </Box>
         </Box>
       </Container>
