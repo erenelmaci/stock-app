50438488e3455f692b04fdc1c81d7a945e650b9a
Eren Elmacı <erenx1623@gmail.com>
2023-03-30 16:56:17 +0300

    commit

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -70,19 +70,6 @@ const Products = () => {
       flex: 1,
     },
   ]
-
-  const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  ]
-
   const [info, setInfo] = useState({
     name: "",
     phone: "",
@@ -95,7 +82,7 @@ const Products = () => {
 
   useEffect(() => {
     getStockData("products")
-  }, [])
+  }, [getStockData])
 
   return (
     <div>
@@ -120,7 +107,7 @@ const Products = () => {
       <Box sx={{ width: "100%" }}>
         <DataGrid
           autoHeight
-          rows={rows}
+          rows={products}
           columns={columns}
           initialState={{
             pagination: {
